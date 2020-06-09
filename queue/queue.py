1cf6bf44e83aa9beca2aaaf0a94e3d9a403d3719
alexmjn <a.jenkneary@gmail.com>
2020-06-09 10:11:41 -0400

    Queues implemented with linked lists

--- queue/queue.py
+++ queue/queue.py
@@ -17,9 +17,9 @@ Stretch: What if you could only use instances of your Stack class to implement t
 # class Queue:
 #     """This class implements a Queue with Python's built-in array list.
 #     """
-#     def __init__(self):
+#     def __init__(self, contents=[]):
 #         self.size = 0
-#         self.contents = []
+#         self.contents = contents
 
 #     def __len__(self):
 #         return self.size
@@ -38,22 +38,29 @@ Stretch: What if you could only use instances of your Stack class to implement t
 
 class Queue:
     """This class implements a queue with linked lists.
+
+    It instantiates an empty queue and builds on LinkedList methods from
+    that class in order to add functionality for adding an item to the queue
+    (and adjusting the tail appropriately) and removing an item from the queue
+    (and adjusting the head appropriately).
     """
-    def __init__(self):
+    def __init__(self, contents=LinkedList()):
+        # Contents is a LinkedList containing nodes
         self.size = 0
-        self.contents = []
+        self.contents = contents
 
     def __len__(self):
         return self.size
 
     def enqueue(self, value):
         self.size += 1
-        self.contents.append(value)
+        self.contents.add_to_tail(value)
 
     def dequeue(self):
-        if self.size == 0:
-            return None
-        else:
+        if self.size >= 1:
             self.size -= 1
-            front_item = self.contents.pop(0)
-            return front_item
+            value = self.contents.remove_head()
+            return value
+
+        else:
+            return None
