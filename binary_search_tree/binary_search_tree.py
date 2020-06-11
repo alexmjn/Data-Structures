1e1acf810fd9ffd4ae89b9095d55982f8dc15011
alexmjn <a.jenkneary@gmail.com>
2020-06-11 12:54:09 -0400

    Binary Search Tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,7 +17,6 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-
         if value < self.value:
             if self.left is None:
                 self.left = BSTNode(value)
@@ -34,27 +33,37 @@ class BSTNode:
 
 
     def contains(self, target):
-        if self.value is None:
-            return False:
-
-        elif self.value == target:
+        if self.value == target:
             return True
 
-        else:
-            contains
+        if target >= self.value:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+        if target < self.value:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
 
     # Return the maximum value found in the tree
     def get_max(self):
-        temp_max = None
+        if self.right is None:
+            return self.value
+        else:
+            return self.right.get_max()
 
-        if self.value > temp_max:
-            temp_max = self.value
 
 
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        fn(self.value)
+        if self.right is not None:
+            self.right.for_each(fn)
+        if self.left is not None:
+            self.left.for_each(fn)
 
     # Part 2 -----------------------
 
